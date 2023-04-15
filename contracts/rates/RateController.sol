222e002f1998e8dadd3880444c5df3d7247e41aa
TRiLeZ <tyler@trilez.com>
2023-04-14 19:26:41 -0700

    Pull IHistoricalRates implementation out of RateController - This improves the readability of RateController

--- contracts/rates/RateController.sol
+++ contracts/rates/RateController.sol
@@ -8,21 +8,13 @@ import "@openzeppelin-v4/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin-v4/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin-v4/contracts/utils/math/SafeCast.sol";
 
-import "./IHistoricalRates.sol";
+import "./HistoricalRates.sol";
 import "./IRateComputer.sol";
 
 /// @title RateController
 /// @notice A contract that periodically computes and stores rates for tokens.
-abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUpdateable, IPeriodic {
+abstract contract RateController is ERC165, HistoricalRates, IRateComputer, IUpdateable, IPeriodic {
     using SafeCast for uint256;
-
-    struct BufferMetadata {
-        uint8 start;
-        uint8 end;
-        uint8 size;
-        uint8 maxSize;
-        bool pauseUpdates;
-    }
 
     struct RateConfig {
         uint64 maxIncrease;
@@ -41,36 +33,8 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
     /// @dev This is a security feature to prevent malicious contracts from updating rates.
     bool public immutable updatersMustBeEoa;
 
-    /// @notice Maps a token to its metadata.
-    mapping(address => BufferMetadata) internal rateBufferMetadata;
-
     /// @notice Maps a token to its rate configuration.
     mapping(address => RateConfig) internal rateConfigs;
-
-    /// @notice Maps a token to a buffer of rates.
-    mapping(address => RateLibrary.Rate[]) internal rateBuffers;
-
-    /// @notice The initial capacity of the rate buffer.
-    uint8 internal immutable initialBufferCardinality;
-
-    /// @notice Event emitted when a rate buffer's capacity is increased past the initial capacity.
-    /// @dev Buffer initialization does not emit an event.
-    /// @param token The token for which the rate buffer's capacity was increased.
-    /// @param oldCapacity The previous capacity of the rate buffer.
-    /// @param newCapacity The new capacity of the rate buffer.
-    event RatesCapacityIncreased(address indexed token, uint256 oldCapacity, uint256 newCapacity);
-
-    /// @notice Event emitted when a rate buffer's capacity is initialized.
-    /// @param token The token for which the rate buffer's capacity was initialized.
-    /// @param capacity The capacity of the rate buffer.
-    event RatesCapacityInitialized(address indexed token, uint256 capacity);
-
-    /// @notice Event emitted when a new rate is pushed to the rate buffer.
-    /// @param token The token for which the rate was pushed.
-    /// @param target The target rate.
-    /// @param current The current rate, which may be different from the target rate if the rate change is capped.
-    /// @param timestamp The timestamp at which the rate was pushed.
-    event RateUpdated(address indexed token, uint256 target, uint256 current, uint256 timestamp);
 
     /// @notice Event emitted when the pause status of rate updates for a token is changed.
     /// @param token The token for which the pause status of rate updates was changed.
@@ -80,34 +44,6 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
     /// @notice Event emitted when the rate configuration for a token is updated.
     /// @param token The token for which the rate configuration was updated.
     event RateConfigUpdated(address indexed token, RateConfig oldConfig, RateConfig newConfig);
-
-    /// @notice An error that is thrown if we try to initialize a rate buffer that has already been initialized.
-    /// @param token The token for which we tried to initialize the rate buffer.
-    error BufferAlreadyInitialized(address token);
-
-    /// @notice An error that is thrown if we try to retrieve a rate at an invalid index.
-    /// @param token The token for which we tried to retrieve the rate.
-    /// @param index The index of the rate that we tried to retrieve.
-    /// @param size The size of the rate buffer.
-    error InvalidIndex(address token, uint256 index, uint256 size);
-
-    /// @notice An error that is thrown if we try to decrease the capacity of a rate buffer.
-    /// @param token The token for which we tried to decrease the capacity of the rate buffer.
-    /// @param amount The capacity that we tried to decrease the rate buffer to.
-    /// @param currentCapacity The current capacity of the rate buffer.
-    error CapacityCannotBeDecreased(address token, uint256 amount, uint256 currentCapacity);
-
-    /// @notice An error that is thrown if we try to increase the capacity of a rate buffer past the maximum capacity.
-    /// @param token The token for which we tried to increase the capacity of the rate buffer.
-    /// @param amount The capacity that we tried to increase the rate buffer to.
-    /// @param maxCapacity The maximum capacity of the rate buffer.
-    error CapacityTooLarge(address token, uint256 amount, uint256 maxCapacity);
-
-    /// @notice An error that is thrown if we try to retrieve more rates than are available in the rate buffer.
-    /// @param token The token for which we tried to retrieve the rates.
-    /// @param size The size of the rate buffer.
-    /// @param minSizeRequired The minimum size of the rate buffer that we require.
-    error InsufficientData(address token, uint256 size, uint256 minSizeRequired);
 
     /// @notice An error that is thrown if we try to set a rate configuration with invalid parameters.
     /// @param token The token for which we tried to set the rate configuration.
@@ -126,9 +62,12 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
     /// @param period_ The period of the rate controller, in seconds. This is the frequency at which rates are updated.
     /// @param initialBufferCardinality_ The initial capacity of the rate buffer.
     /// @param updatersMustBeEoa_ True if all rate updaters must be EOA accounts; false otherwise.
-    constructor(uint32 period_, uint8 initialBufferCardinality_, bool updatersMustBeEoa_) {
+    constructor(
+        uint32 period_,
+        uint8 initialBufferCardinality_,
+        bool updatersMustBeEoa_
+    ) HistoricalRates(initialBufferCardinality_) {
         period = period_;
-        initialBufferCardinality = initialBufferCardinality_;
         updatersMustBeEoa = updatersMustBeEoa_;
     }
 
@@ -229,85 +168,6 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
         return computeRateInternal(token);
     }
 
-    /// @inheritdoc IHistoricalRates
-    function getRateAt(address token, uint256 index) external view virtual override returns (RateLibrary.Rate memory) {
-        BufferMetadata memory meta = rateBufferMetadata[token];
-
-        if (index >= meta.size) {
-            revert InvalidIndex(token, index, meta.size);
-        }
-
-        uint256 bufferIndex = meta.end < index ? meta.end + meta.size - index : meta.end - index;
-
-        return rateBuffers[token][bufferIndex];
-    }
-
-    /// @inheritdoc IHistoricalRates
-    function getRates(
-        address token,
-        uint256 amount
-    ) external view virtual override returns (RateLibrary.Rate[] memory) {
-        return getRatesInternal(token, amount, 0, 1);
-    }
-
-    /// @inheritdoc IHistoricalRates
-    function getRates(
-        address token,
-        uint256 amount,
-        uint256 offset,
-        uint256 increment
-    ) external view virtual override returns (RateLibrary.Rate[] memory) {
-        return getRatesInternal(token, amount, offset, increment);
-    }
-
-    /// @inheritdoc IHistoricalRates
-    function getRatesCount(address token) external view override returns (uint256) {
-        return rateBufferMetadata[token].size;
-    }
-
-    /// @inheritdoc IHistoricalRates
-    function getRatesCapacity(address token) external view virtual override returns (uint256) {
-        uint256 maxSize = rateBufferMetadata[token].maxSize;
-        if (maxSize == 0) return initialBufferCardinality;
-
-        return maxSize;
-    }
-
-    /// @notice Sets the capacity of the rate buffer for a token. Only callable by the admin because the
-    ///   updating logic is O(n) on the capacity. Only callable when the rate config is set.
-    /// @param amount The new capacity of rates for the token. Must be greater than the current capacity, but
-    ///   less than 256.
-    /// @inheritdoc IHistoricalRates
-    function setRatesCapacity(address token, uint256 amount) external virtual {
-        checkSetRatesCapacity();
-
-        BufferMetadata storage meta = rateBufferMetadata[token];
-        if (meta.maxSize == 0) {
-            // Buffer is not initialized yet
-            // Buffer can only be initialized when the rate config is set
-            revert MissingConfig(token);
-        }
-
-        if (amount < meta.maxSize) revert CapacityCannotBeDecreased(token, amount, meta.maxSize);
-        if (amount > type(uint8).max) revert CapacityTooLarge(token, amount, type(uint8).max);
-
-        RateLibrary.Rate[] storage rateBuffer = rateBuffers[token];
-
-        // Add new slots to the buffer
-        uint256 capacityToAdd = amount - meta.maxSize;
-        for (uint256 i = 0; i < capacityToAdd; ++i) {
-            // Push a dummy rate with non-zero values to put most of the gas cost on the caller
-            rateBuffer.push(RateLibrary.Rate({target: 1, current: 1, timestamp: 1}));
-        }
-
-        if (meta.maxSize != amount) {
-            emit RatesCapacityIncreased(token, meta.maxSize, amount);
-
-            // Update the metadata
-            meta.maxSize = uint8(amount);
-        }
-    }
-
     /// @inheritdoc IPeriodic
     function granularity() external view virtual override returns (uint256) {
         return 1;
@@ -368,6 +228,26 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
             super.supportsInterface(interfaceId);
     }
 
+    /**
+     * @dev Internal function to set the capacity of the rate buffer for a token. Only callable by the admin because the
+     * updating logic is O(n) on the capacity. Only callable when the rate config is set.
+     * @param token The token for which to set the new capacity.
+     * @param amount The new capacity of rates for the token. Must be greater than the current capacity, but
+     * less than 256.
+     */
+    function _setRatesCapacity(address token, uint256 amount) internal virtual override {
+        checkSetRatesCapacity();
+
+        BufferMetadata storage meta = rateBufferMetadata[token];
+        if (meta.maxSize == 0) {
+            // Buffer is not initialized yet
+            // Buffer can only be initialized when the rate config is set
+            revert MissingConfig(token);
+        }
+
+        super._setRatesCapacity(token, amount);
+    }
+
     function willAnythingChange(bytes memory data) internal view virtual returns (bool) {
         address token = abi.decode(data, (address));
 
@@ -379,7 +259,7 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
         // All current rates in the buffer should match the next rate. Otherwise, the rate will change.
         // We don't check target rates because if the rate is capped, the current rate may never reach the target rate.
         (, uint64 nextRate) = computeRateAndClamp(token);
-        RateLibrary.Rate[] memory rates = getRatesInternal(token, meta.size, 0, 1);
+        RateLibrary.Rate[] memory rates = _getRates(token, meta.size, 0, 1);
         for (uint256 i = 0; i < rates.length; ++i) {
             if (rates[i].current != nextRate) return true;
         }
@@ -399,82 +279,6 @@ abstract contract RateController is ERC165, IHistoricalRates, IRateComputer, IUp
         }
 
         return rateBuffers[token][meta.end];
-    }
-
-    function getRatesInternal(
-        address token,
-        uint256 amount,
-        uint256 offset,
-        uint256 increment
-    ) internal view virtual returns (RateLibrary.Rate[] memory) {
-        if (amount == 0) return new RateLibrary.Rate[](0);
-
-        BufferMetadata memory meta = rateBufferMetadata[token];
-        if (meta.size <= (amount - 1) * increment + offset)
-            revert InsufficientData(token, meta.size, (amount - 1) * increment + offset + 1);
-
-        RateLibrary.Rate[] memory observations = new RateLibrary.Rate[](amount);
-
-        uint256 count = 0;
-
-        for (
-            uint256 i = meta.end < offset ? meta.end + meta.size - offset : meta.end - offset;
-            count < amount;
-            i = (i < increment) ? (i + meta.size) - increment : i - increment
-        ) {
-            observations[count++] = rateBuffers[token][i];
-        }
-
-        return observations;
-    }
-
-    function initializeBuffers(address token) internal virtual {
-        if (rateBuffers[token].length != 0) {
-            revert BufferAlreadyInitialized(token);
-        }
-
-        BufferMetadata storage meta = rateBufferMetadata[token];
-
-        // Initialize the buffers
-        RateLibrary.Rate[] storage observationBuffer = rateBuffers[token];
-
-        for (uint256 i = 0; i < initialBufferCardinality; ++i) {
-            observationBuffer.push();
-        }
-
-        // Initialize the metadata
-        meta.start = 0;
-        meta.end = 0;
-        meta.size = 0;
-        meta.maxSize = initialBufferCardinality;
-        meta.pauseUpdates = false;
-
-        emit RatesCapacityInitialized(token, meta.maxSize);
-    }
-
-    function push(address token, RateLibrary.Rate memory rate) internal virtual {
-        BufferMetadata storage meta = rateBufferMetadata[token];
-
-        if (meta.size == 0) {
-            if (meta.maxSize == 0) {
-                // Initialize the buffers
-                initializeBuffers(token);
-            }
-        } else {
-            meta.end = (meta.end + 1) % meta.maxSize;
-        }
-
-        rateBuffers[token][meta.end] = rate;
-
-        emit RateUpdated(token, rate.target, rate.current, block.timestamp);
-
-        if (meta.size < meta.maxSize && meta.end == meta.size) {
-            // We are at the end of the array and we have not yet filled it
-            meta.size++;
-        } else {
-            // start was just overwritten
-            meta.start = (meta.start + 1) % meta.size;
-        }
     }
 
     function computeRateInternal(address token) internal view virtual returns (uint64) {
