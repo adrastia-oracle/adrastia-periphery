25657adcb2f80f1fabb6079e5bd81714a3d4a7ee
Tyler Loewen <tyler@trilez.com>
2023-06-09 13:20:40 -0700

    Add ManagedMedianFilteringOracle and ManagedPriceVolatilityOracle

---
+++ contracts/oracles/ManagedMedianFilteringOracle.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "@adrastia-oracle/adrastia-core/contracts/oracles/MedianFilteringOracle.sol";
+
+import "./bases/ManagedHistoricalAggregatorOracleBase.sol";
+
+contract ManagedMedianFilteringOracle is MedianFilteringOracle, ManagedHistoricalAggregatorOracleBase {
+    constructor(
+        IHistoricalOracle source_,
+        uint256 observationAmount_,
+        uint256 observationOffset_,
+        uint256 observationIncrement_
+    )
+        MedianFilteringOracle(source_, observationAmount_, observationOffset_, observationIncrement_)
+        ManagedHistoricalAggregatorOracleBase(source_, observationAmount_, observationOffset_, observationIncrement_)
+    {}
+
+    function canUpdate(bytes memory data) public view virtual override returns (bool) {
+        // Return false if the message sender is missing the required role
+        if (!hasRole(Roles.ORACLE_UPDATER, address(0)) && !hasRole(Roles.ORACLE_UPDATER, msg.sender)) return false;
+
+        return super.canUpdate(data);
+    }
+
+    function update(bytes memory data) public virtual override onlyRoleOrOpenRole(Roles.ORACLE_UPDATER) returns (bool) {
+        return super.update(data);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControlEnumerable, HistoricalAggregatorOracle) returns (bool) {
+        return
+            AccessControlEnumerable.supportsInterface(interfaceId) ||
+            HistoricalAggregatorOracle.supportsInterface(interfaceId);
+    }
+
+    function _source() internal view virtual override returns (IHistoricalOracle) {
+        return config.source;
+    }
+
+    function _observationAmount() internal view virtual override returns (uint256) {
+        return config.observationAmount;
+    }
+
+    function _observationOffset() internal view virtual override returns (uint256) {
+        return config.observationOffset;
+    }
+
+    function _observationIncrement() internal view virtual override returns (uint256) {
+        return config.observationIncrement;
+    }
+}
