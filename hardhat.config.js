24461db0bf5edd4cccded4b24029fcc1c7e36485
Tyler Loewen <tyler@trilez.com>
2023-07-28 12:44:34 -0700

    Add network configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -38,12 +38,42 @@ module.exports = {
         polygon: {
             chainId: 137,
             url: process.env.POLYGON_URL || "",
+            accounts: [process.env.PRIVATE_KEY_DEPLOYER || ""],
+            gasMultiplier: 2,
+        },
+        polygonZkEVM: {
+            chainId: 1101,
+            url: process.env.POLYGONZKEVM_URL || "",
+            accounts: [process.env.PRIVATE_KEY_DEPLOYER || ""],
+        },
+        arbitrumOne: {
+            chainId: 42161,
+            url: process.env.ARBITRUMONE_URL || "",
+            accounts: [process.env.PRIVATE_KEY_DEPLOYER || ""],
+        },
+        optimisticEthereum: {
+            chainId: 10,
+            url: process.env.OPTIMISM_URL || "",
+            accounts: [process.env.PRIVATE_KEY_DEPLOYER || ""],
         },
     },
     etherscan: {
         apiKey: {
             mainnet: process.env.ETHERSCAN_API_KEY,
             polygon: process.env.POLYGONSCAN_API_KEY,
+            polygonZkEVM: process.env.POLYGONSCANZKEVM_API_KEY,
+            arbitrumOne: process.env.ARBISCAN_API_KEY,
+            optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_API_KEY,
         },
+        customChains: [
+            {
+                network: "polygonZkEVM",
+                chainId: 1101,
+                urls: {
+                    apiURL: "https://api-zkevm.polygonscan.com/api",
+                    browserURL: "https://zkevm.polygonscan.com",
+                },
+            },
+        ],
     },
 };
