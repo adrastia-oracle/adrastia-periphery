3aa0c71ad76ab9080f80e2316a6973832c23062e
Tyler Loewen <tyler@trilez.com>
2023-08-03 22:01:40 -0700

    Add tests for MutatedValueComputer

---
+++ contracts/test/rates/computers/MutatedValueComputerStub.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "../../../rates/computers/MutatedValueComputer.sol";
+
+contract MutatedValueComputerStub is MutatedValueComputer {
+    mapping(address => uint256) public values;
+
+    bool public revertOnSetConfig;
+
+    constructor(uint32 defaultOneXScalar_) MutatedValueComputer(defaultOneXScalar_) {
+        revertOnSetConfig = false;
+    }
+
+    function stubSetValue(address token, uint256 value) public {
+        values[token] = value;
+    }
+
+    function stubSetRevertOnSetConfig(bool revertOnSetConfig_) public {
+        revertOnSetConfig = revertOnSetConfig_;
+    }
+
+    function getValue(address token) internal view virtual override returns (uint256) {
+        return values[token];
+    }
+
+    function checkSetConfig() internal view virtual override {
+        if (revertOnSetConfig) revert("revertOnSetConfig");
+    }
+}
