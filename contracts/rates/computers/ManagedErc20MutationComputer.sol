d298eb2803e7b6c65752a920fbd3845329bd2b89
Tyler Loewen <tyler@trilez.com>
2023-08-09 18:37:31 -0700

    Add Compound III (Comet) supply, borrow, and collateral cap computers

---
+++ contracts/rates/computers/ManagedErc20MutationComputer.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "@openzeppelin-v4/contracts/access/AccessControlEnumerable.sol";
+
+import "./Erc20MutationComputer.sol";
+import "../../access/Roles.sol";
+
+/**
+ * @title CTokenMutationComputer
+ * @notice Abstract contract for computing mutated values from cTokens.
+ * @dev Extend this contract and implement the extractValueFromToken function to use it.
+ */
+abstract contract ManagedErc20MutationComputer is Erc20MutationComputer, AccessControlEnumerable {
+    /**
+     * @notice Constructs a new CTokenMutationComputer instance.
+     * @param defaultOneXScalar_ The default scalar value to represent 1x. Recommended value: 1,000,000.
+     * @param defaultDecimals_ The default number of decimals for the underlying tokens. Recommended value: 18.
+     * @param decimalsOffset_ The offset to apply when scaling the value from the token.
+     */
+    constructor(
+        uint32 defaultOneXScalar_,
+        uint8 defaultDecimals_,
+        int8 decimalsOffset_
+    ) Erc20MutationComputer(defaultOneXScalar_, defaultDecimals_, decimalsOffset_) {
+        initializeRoles();
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControlEnumerable, MutatedValueComputer) returns (bool) {
+        return
+            AccessControlEnumerable.supportsInterface(interfaceId) ||
+            MutatedValueComputer.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @notice Checks if the sender has the required role to set the config, namely, the POOL_ADMIN role.
+     */
+    function checkSetConfig() internal view virtual override onlyRole(Roles.RATE_ADMIN) {}
+
+    /**
+     * @notice Initializes the roles hierarchy.
+     * @dev Sets up the roles and their hierarchy:
+     *          ADMIN
+     *            |
+     *        RATE_ADMIN
+     * @dev The ADMIN role is set up with msg.sender as the initial admin.
+     */
+    function initializeRoles() internal virtual {
+        // Setup admin role, setting msg.sender as admin
+        _setupRole(Roles.ADMIN, msg.sender);
+        _setRoleAdmin(Roles.ADMIN, Roles.ADMIN);
+
+        // Set admin of RATE_ADMIN as ADMIN
+        _setRoleAdmin(Roles.RATE_ADMIN, Roles.ADMIN);
+
+        // Hierarchy:
+        // ADMIN
+        //   - RATE_ADMIN
+    }
+}
---
+++ contracts/rates/computers/proto/compound/CometBorrowMutationComputer.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "../../ManagedErc20MutationComputer.sol";
+
+interface IComet {
+    function totalBorrow() external view returns (uint256);
+
+    function baseToken() external view returns (address);
+}
+
+/**
+ * @title CometBorrowMutationComputer
+ * @notice A Erc20MutationComputer implementation that computes mutated values using the total base asset borrowed
+ *   amount in the Comet contract.
+ */
+contract CometBorrowMutationComputer is ManagedErc20MutationComputer {
+    address public immutable comet;
+
+    address public immutable baseToken;
+
+    /**
+     * @notice Constructs a new CometBorrowMutationComputer instance.
+     * @param defaultOneXScalar_ The default scalar value to represent 1x. Recommended value: 1,000,000.
+     * @param defaultDecimals_ The default number of decimals for the tokens. Recommended value: 18.
+     * @param decimalsOffset_ The offset to apply when scaling the value from the token.
+     * @param comet_ The address of the Comet contract.
+     */
+    constructor(
+        address comet_,
+        uint32 defaultOneXScalar_,
+        uint8 defaultDecimals_,
+        int8 decimalsOffset_
+    ) ManagedErc20MutationComputer(defaultOneXScalar_, defaultDecimals_, decimalsOffset_) {
+        comet = comet_;
+        baseToken = IComet(comet_).baseToken();
+    }
+
+    /**
+     * @notice Extracts the base asset borrowed amount (plus interest) in the Comet contract.
+     * @return The extracted base asset borrowed amount (plus interest).
+     */
+    function extractValueFromToken(address token) internal view virtual override returns (uint256) {
+        if (token != baseToken) {
+            // It can be misleading to return a value for a token that doesn't match the base token, so we revert.
+            revert InvalidInput(token);
+        }
+
+        return IComet(comet).totalBorrow();
+    }
+}
