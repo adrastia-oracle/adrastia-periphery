8916ef783873fbf8af232cf5f28535c3bc601cb3
Tyler Loewen <tyler@trilez.com>
2023-08-29 13:54:04 -0700

    Add OracleMutationComputer and ManagedOracleMutationComputer

---
+++ contracts/rates/computers/ManagedOracleMutationComputer.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "@openzeppelin-v4/contracts/access/AccessControlEnumerable.sol";
+
+import "./OracleMutationComputer.sol";
+import "../../access/Roles.sol";
+
+contract ManagedOracleMutationComputer is OracleMutationComputer, AccessControlEnumerable {
+    constructor(
+        IOracle oracle_,
+        uint256 dataSlot_,
+        uint32 defaultOneXScalar_,
+        int8 decimalsOffset_
+    ) OracleMutationComputer(oracle_, dataSlot_, defaultOneXScalar_, decimalsOffset_) {
+        initializeRoles();
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControlEnumerable, MutatedValueComputer) returns (bool) {
+        return
+            AccessControlEnumerable.supportsInterface(interfaceId) ||
+            MutatedValueComputer.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @notice Checks if the sender has the required role to set the config, namely, the POOL_ADMIN role.
+     */
+    function checkSetConfig() internal view virtual override onlyRole(Roles.RATE_ADMIN) {}
+
+    /**
+     * @notice Initializes the roles hierarchy.
+     * @dev Sets up the roles and their hierarchy:
+     *          ADMIN
+     *            |
+     *        RATE_ADMIN
+     * @dev The ADMIN role is set up with msg.sender as the initial admin.
+     */
+    function initializeRoles() internal virtual {
+        // Setup admin role, setting msg.sender as admin
+        _setupRole(Roles.ADMIN, msg.sender);
+        _setRoleAdmin(Roles.ADMIN, Roles.ADMIN);
+
+        // Set admin of RATE_ADMIN as ADMIN
+        _setRoleAdmin(Roles.RATE_ADMIN, Roles.ADMIN);
+
+        // Hierarchy:
+        // ADMIN
+        //   - RATE_ADMIN
+    }
+}
---
+++ contracts/rates/computers/OracleMutationComputer.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "@adrastia-oracle/adrastia-core/contracts/interfaces/IOracle.sol";
+
+import "./Erc20MutationComputer.sol";
+
+abstract contract OracleMutationComputer is Erc20MutationComputer {
+    uint256 public constant DATA_SLOT_PRICE = 1;
+    uint256 public constant DATA_SLOT_LIQUIDITY_TOKEN = 2;
+    uint256 public constant DATA_SLOT_LIQUIDITY_QUOTETOKEN = 3;
+
+    IOracle public immutable oracle;
+
+    uint256 public immutable dataSlot;
+
+    uint8 internal immutable liquidityDecimals;
+    uint8 internal immutable priceDecimals;
+
+    error InvalidDataSlot(uint256 slot);
+
+    constructor(
+        IOracle oracle_,
+        uint256 dataSlot_,
+        uint32 defaultOneXScalar_,
+        int8 decimalsOffset_
+    ) Erc20MutationComputer(defaultOneXScalar_, 0, decimalsOffset_) {
+        if (
+            dataSlot_ != DATA_SLOT_PRICE &&
+            dataSlot_ != DATA_SLOT_LIQUIDITY_TOKEN &&
+            dataSlot_ != DATA_SLOT_LIQUIDITY_QUOTETOKEN
+        ) {
+            revert InvalidDataSlot(dataSlot_);
+        }
+
+        oracle = oracle_;
+        dataSlot = dataSlot_;
+
+        liquidityDecimals = oracle_.liquidityDecimals();
+        priceDecimals = oracle_.quoteTokenDecimals();
+    }
+
+    function getTokenDecimalsOrDefault(address) internal view virtual override returns (uint8) {
+        if (dataSlot == DATA_SLOT_PRICE) {
+            return priceDecimals;
+        } else {
+            return liquidityDecimals;
+        }
+    }
+
+    function extractValueFromToken(address token) internal view virtual override returns (uint256 result) {
+        if (dataSlot == DATA_SLOT_PRICE) {
+            // Fetch price
+            (result) = oracle.consultPrice(token);
+        } else if (dataSlot == DATA_SLOT_LIQUIDITY_TOKEN) {
+            // Fetch tokenLiquidity
+            (result, ) = oracle.consultLiquidity(token);
+        } else {
+            // Assume this case is for DATA_SLOT_LIQUIDITY_QUOTETOKEN b/c the constructor enforces this
+            // Fetch quoteTokenLiquidity
+            (, result) = oracle.consultLiquidity(token);
+        }
+    }
+}
